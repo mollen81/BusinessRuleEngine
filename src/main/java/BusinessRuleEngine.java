2f9af956c3717cfcb9bf7d33e1be0fe848172266
mollen <stepanpanenko@gmail.com>
2026-03-11 23:18:02 +0300

    BusinessRuleEngine.java and Action.java functional interface

---
+++ src/main/java/BusinessRuleEngine.java
@@ -0,0 +1,14 @@
+public class BusinessRuleEngine {
+
+    public void addRule(Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+}
